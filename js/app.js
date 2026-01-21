5ef86a40fae617f5992364d8084f8391af3fba2a
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-21 10:17:29 +0100

    add try catch part in async function getBackgroundPhoto

--- js/app.js
+++ js/app.js
@@ -3,19 +3,29 @@ import { CONFIG } from '/config/main-config.js';
 getBackgroundPhoto();
 
 function setHeaderPhoto(imageURL) {
-  document.getElementById('hero').style.backgroundImage = `url(${imageURL})`;
+  document.getElementById('hero').style.backgroundImage = ` url(${imageURL})`;
 }
 
 async function getBackgroundPhoto() {
-  const baseURL = `https://api.unsplash.com/photos/random/?query=movie`;
-  const response = await fetch(baseURL, {
-    headers: {
-      Authorization: `Client-ID ${CONFIG.UNSLPASH_API_KEY}`,
-    },
-  });
+  try {
+    // get a random movie image with the unsplash API
+    const baseURL = `https://api.unsplash.com/photos/random/!!!?query=movie`;
 
-  const data = await response.json();
-  const imageUnsplash = data.urls.regular;
+    const response = await fetch(baseURL, {
+      headers: {
+        Authorization: `Client-ID ${CONFIG.UNSLPASH_API_KEY}`,
+      },
+    });
+    const data = await response.json();
+    const imageUnsplash = data.urls.regular;
 
-  setHeaderPhoto(imageUnsplash);
+    // set the background image
+    // setHeaderPhoto(imageUnsplash);
+  } catch (err) {
+    console.log(err.message);
+
+    // set default header image
+    const defaultBackgroundImage = '/assets/images/movies-poster.jpg';
+    setHeaderPhoto(defaultBackgroundImage);
+  }
 }
