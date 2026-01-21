8dae63cee81f2ea09a92ff01bfbc8f24018a35df
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-21 12:16:19 +0100

    create a method setMoviesInformation to get all the informaton for the OMDB api

--- js/movies.js
+++ js/movies.js
@@ -1,12 +1,47 @@
 import { CONFIG } from '/config/main-config.js';
 
+export async function setMoviesInformation(moviesOMDB) {
+  const moviesPromises = moviesOMDB.map((movie) => getMovieById(movie.imdbID));
+  console.log(moviesPromises);
+
+  const movies = Promise.all(moviesPromises);
+
+  return movies;
+}
+
 export async function getMoviesBySearch(movie) {
   try {
     const searchMovieTemplate = movie.trim().replaceAll(' ', '+');
 
-    const baseURL = `https://www.omdbapi.com/?s=${searchMovieTemplate}&type=movie&apikey=${CONFIG.OMDB_API_KEY}`; //&apikey=${CONFIG.OMDB_API_KEY}
+    const baseURL = `https://www.omdbapi.com/?s=${searchMovieTemplate}&type=movie&apikey=${CONFIG.OMDB_API_KEY}`;
     const response = await fetch(baseURL);
     const data = await response.json();
+
+    if (!response.ok) {
+      throw Error('Network error');
+    }
+
+    if (data.Response === 'False') {
+      throw Error(data.Error);
+    }
+
+    return data.Search;
+  } catch (err) {
+    console.error(`getMoviesBySearch(${movie}):`, err.message);
+
+    return [];
+  }
+}
+
+async function getMovieById(movieId) {
+  try {
+    const baseURL = `https://www.omdbapi.com/?i=${movieId}&type=movie&plot=full&apikey=${CONFIG.OMDB_API_KEY}`;
+    const response = await fetch(baseURL);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw Error('Network error');
+    }
 
     if (data.Response === 'False') {
       throw Error(data.Error);
@@ -14,8 +49,8 @@ export async function getMoviesBySearch(movie) {
 
     return data;
   } catch (err) {
-    console.error(err.message);
+    console.error(`getMovieById(${movieId}):`, err.message);
 
-    return [];
+    return null;
   }
 }
