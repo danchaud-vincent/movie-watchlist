7c34aabca893091ede8227d14da71057d5af5e2b
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-22 10:28:21 +0100

    add addMovie and removeMovie methods in watchlist service

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,9 +1,21 @@
 const STORAGE_WATCHLIST_KEY = 'watchlist';
 
+let watchlist = loadWatchlist();
+
 function loadWatchlist() {
   return JSON.parse(localStorage.getItem(STORAGE_WATCHLIST_KEY)) || [];
 }
 
 function saveWatchlist(watchlist) {
   localStorage.setItem(STORAGE_WATCHLIST_KEY, JSON.stringify(watchlist));
+}
+
+function addMovieToWatchlist(movie) {
+  watchlist.push(movie);
+  saveWatchlist(watchlist);
+}
+
+function removeMovieFromWatchlist(movieId) {
+  watchlist.filter((movie) => movie.imdbID === movieId);
+  saveWatchlist(watchlist);
 }
