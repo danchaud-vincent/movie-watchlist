7e569ef2b48f462b31322dadb34d9ad222d42bdf
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-23 11:36:11 +0100

    add handleToggleMovieInWatchlist method in watchlist.js

--- js/pages/watchlist.js
+++ js/pages/watchlist.js
@@ -1,6 +1,6 @@
 import { loadRandomHeroImage } from '../services/unsplashService.js';
 import { renderMovies } from '../components/renderMovies.js';
-import { loadWatchlist } from '../services/watchlistService.js';
+import { loadWatchlist, toggleMovieInWatchlist } from '../services/watchlistService.js';
 
 // set hero background image
 loadRandomHeroImage('hero');
@@ -8,3 +8,30 @@ loadRandomHeroImage('hero');
 // render the watchlist
 const watchlist = loadWatchlist();
 renderMovies(watchlist, 'watchlist');
+
+// ------------ EVENT LISTENER ------------
+document.addEventListener('click', handleGlobalClick);
+
+function handleGlobalClick(e) {
+  if (e.target.classList.contains('watchlist-btn')) {
+    handleToggleMovieInWatchlist(e);
+  }
+}
+
+function handleToggleMovieInWatchlist(e) {
+  const movieContainer = e.target.closest('.movie-container');
+  const movieId = movieContainer.dataset.movieId;
+
+  const currentWatchlist = loadWatchlist();
+
+  const movie = currentWatchlist.find((m) => m.imdbID === movieId);
+
+  if (!movie) {
+    return;
+  }
+
+  const updatedWatchlist = toggleMovieInWatchlist(movie);
+
+  // render movies
+  renderMovies(updatedWatchlist, 'watchlist');
+}
