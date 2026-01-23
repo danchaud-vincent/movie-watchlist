bac52e3827ed1501b9dfcf71934d691ce44b5bf1
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-23 10:45:08 +0100

    add code in renderMovies.js

---
+++ js/components/renderMovies.js
@@ -0,0 +1,18 @@
+import { getMessageContainer } from './messageComponent.js';
+
+export function renderMovies(movies, pageType) {
+  const container = document.getElementById('movies');
+
+  if (!movies.length) {
+    const messageContent = getMessageContainer(pageType);
+    container.innerHTML = messageContent;
+    return;
+  }
+
+  container.innerHTML = movies.map((movie) => createMovieCard(movie)).join('');
+
+  // init plots
+  document.querySelectorAll('.movie-plot').forEach((plot) => {
+    truncateTextToggle(plot, 100);
+  });
+}
