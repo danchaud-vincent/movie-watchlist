e2874f77b12a22f6e22ebcd3e37d8143be2d9045
Vincent Danchaud <vdanchaud@gmail.com>
2026-01-23 11:32:52 +0100

    add toggleMovieInWatchlist method

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,6 +1,6 @@
 import { renderMovies } from '../components/renderMovies.js';
-import { getMoviesWithDetailsBySearch, mergeMoviesWithWatchlist } from '../services/moviesService.js';
-import { loadWatchlist } from '../services/watchlistService.js';
+import { getMoviesWithDetailsBySearch, loadMovies, mergeMoviesWithWatchlist } from '../services/moviesService.js';
+import { loadWatchlist, toggleMovieInWatchlist } from '../services/watchlistService.js';
 import { loadRandomHeroImage } from '../services/unsplashService.js';
 
 const searchForm = document.getElementById('form-search-movie');
@@ -10,6 +10,34 @@ loadRandomHeroImage('hero');
 
 // ------------ EVENT LISTENER ------------
 searchForm.addEventListener('submit', handleMovieSearch);
+document.addEventListener('click', handleGlobalClick);
+
+function handleGlobalClick(e) {
+  if (e.target.classList.contains('watchlist-btn')) {
+    handleToggleMovieInWatchlist(e);
+  }
+}
+
+function handleToggleMovieInWatchlist(e) {
+  const movieContainer = e.target.closest('.movie-container');
+  const movieId = movieContainer.dataset.movieId;
+
+  const movie = loadMovies().find((m) => m.imdbID === movieId);
+  console.log(movie);
+
+  if (!movie) {
+    return;
+  }
+
+  const updatedWatchlist = toggleMovieInWatchlist(movie);
+
+  // render the movies updated
+  const movies = loadMovies();
+  const moviesWithWatchlistStatus = mergeMoviesWithWatchlist(movies, updatedWatchlist);
+
+  // render movies
+  renderMovies(moviesWithWatchlistStatus, 'index');
+}
 
 async function handleMovieSearch(e) {
   e.preventDefault();
